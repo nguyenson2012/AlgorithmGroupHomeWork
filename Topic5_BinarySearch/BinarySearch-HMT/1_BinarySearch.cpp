e0ef704ca9b574fc1389b8b9140b484cf16a5f6d
Hoang Minh Tuan <tibanh03@gmail.com>
2024-08-27 15:22:19 +0700

    b1_BiSearch

---
+++ Topic5_BinarySearch/BinarySearch-HMT/1_BinarySearch.cpp
@@ -0,0 +1,21 @@
+// Time complexity: O(log n)
+// Space complexity: O(1)
+// Explanation: The code explains itself =))
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        while(left <= right){
+            int mid  = (left + right) / 2;
+            if(target == nums[mid]){
+                return mid;
+            }else if(target < nums[mid]){
+                right = mid - 1; 
+            }else{
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+};
